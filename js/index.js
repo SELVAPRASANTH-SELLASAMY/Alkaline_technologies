67762b9b83cff8ed44acb5bdaad973edde3fc63d
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-07-09 09:55:18 +0530

    initial commit

--- js/index.js
+++ js/index.js
@@ -10,3 +10,22 @@ const main = document.querySelector('main');
 main.onclick = function(){
     nav.classList.remove('show-nav');
 }
+
+const techStackContainers = document.querySelectorAll('.tech-stacks .stacks');
+const techStackNavs = document.querySelectorAll('.tech-stacks nav li');
+techStackNavs.forEach((techStackNav)=>{
+    techStackNav.onclick = function(){
+        const id = techStackNav.innerText.toLowerCase().replace(" ","_");
+        var targetId = document.getElementById(`${id}`);
+        techStackNavs.forEach(techStackNav => {
+            techStackNav.classList.remove('active');
+            const id = techStackNav.innerText.toLowerCase().replace(" ","_");
+            var targetId = document.getElementById(`${id}`);
+            targetId.classList.remove('display-stacks');
+        });
+        targetId.classList.add('display-stacks');
+        techStackNav.classList.add('active');
+    };
+});
+
+//TODO:Logic should be simplified
